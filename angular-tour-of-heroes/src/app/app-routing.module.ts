437ed618ab7cbb2f37224526bc8e54d9b613b0a1
shengwei0515 <shengwei199505@gmail.com>
2021-10-28 01:32:19 +0800

    class5: add router

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
